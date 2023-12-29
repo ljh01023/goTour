7c0228ba436f051c1c4ed5290bc881bea6e1c545
Lee Ji Hyun <ljhjh01023@gmail.com>
2023-12-29 10:48:21 +0900

    add readme

--- src/components/Nolist.js
+++ src/components/Nolist.js
@@ -1,8 +1,8 @@
 // 리스트의 개수가 0일때 보여지는 component
 export default function NoList() {
   return (
-    <div className="noList">
-      <i className="fa-regular fa-rectangle-list"></i>
+    <div className='noList'>
+      <i className='fa-regular fa-rectangle-list'></i>
       <p>등록된 여행지가 없습니다.</p>
     </div>
   );
